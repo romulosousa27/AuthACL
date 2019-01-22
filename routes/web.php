8d30144824aca3e3bddb8e9b573c0014cedc50ec
Romulo Sousa <romulosousa27@hotmail.com>
2019-01-21 23:16:48 -0300

    Rotas de autenticação

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+    Route::get('/', function () {
+        return view('welcome');
+    });
+*/
+Route::get('/', 'UserController@home')->name('home');
+Route::get('/register', 'UserController@register')->name('register');
+Route::post('/store', 'UserController@store')->name('store');
+
+Route::get('/login', 'AuthenticateController@login')->name('login');
+Route::post('/logar', 'AuthenticateController@logar')->name('logar');
+Route::get('/logout', 'AuthenticateController@logout')->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', 'AuthenticateController@dashboard')->name('dashboard');
 });
