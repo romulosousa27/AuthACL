9b074f1c96d8aa8f3695bb347050dd09c67fe645
Romulo Sousa <romulosousa27@hotmail.com>
2019-01-21 23:17:25 -0300

    UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
-class UserController extends Controller
-{
-    //
+class UserController extends Controller {
+        
+    private $user;
+    
+    /**
+     *
+     * @param User $user
+     */
+    public function __construct(User $user) {
+        $this->user = $user;
+    }
+
+    public function home() {
+        
+        return view('home');
+    }
+
+
+    /**
+     * Retornando a view para formulario de cadastro
+     *
+     * @return void
+     */
+    public function register() {
+        
+        return view('users.register');
+    }
+
+    /**
+     * Criando um novo usuário
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function store(Request $request) {
+        
+        $data = $request->all();
+        $data['password'] = bcrypt($data['password']);
+        
+        $this->user->create($data);
+
+        return redirect()->route('home');
+    }
 }
