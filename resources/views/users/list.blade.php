3df7f7740a459059f470df1f84aaca33bdcbc4ba
Romulo Sousa <romulosousa27@hotmail.com>
2019-01-25 23:03:43 -0300

    Listar todos os usuario Restringir permissao de edita perfil

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -11,21 +11,24 @@
                             <th>Nome</th>
                             <th>E-mail</th>
                             <th>Login</th>
+                            <th>Cargo</th>
                             <th>Ações</th>
                         </tr>
                     </thead>            
                     <tbody>            
                         @foreach($users as $user)
-                            <tr>
-                                <td>{{$user->name}}</td>
-                                <td>{{$user->email}}</td>
-                                <td>{{$user->login}}</td>
-                                @can('update', App\Usuario::class)
-                                    <a href="{{route('editar', $usuario->id)}}"><i class="glyphicon glyphicon-pencil"></i></a>
-                                @endcan
-                                </td>                                
-                            </tr>                         
-                        @endforeach                                
+                        <tr>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>{{$user->login}}</td>
+                            <td>{{$user->permission}}</td>
+                            <td>
+                            {{-- @can('update', App\Models\User::class) --}}
+                                <a href="{{route('edit', $user->id)}}"><i class="glyphicon glyphicon-pencil"></i></a>
+                            {{-- @endcan --}}
+                            </td>                             
+                        </tr>                             
+                        @endforeach 
                     </tbody>
                 </table> 
             </div> 
